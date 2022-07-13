0611ceed21d5f738da5deeea4d7e7402b59b7483
Niamh <niamh.brockbank@gmail.com>
2022-07-13 16:10:58 +0100

    Update App content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import FavouritePlaces from "./components/FavouritePlaces";
+import PageHeader from "./components/PageHeader";
 import { greet } from "./utils/greet";
+import PageFooter from "./components/PageFooter";
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+  <>
+  <FavouritePlaces />;
+  <PageHeader />;
+  <PageFooter />
+  </>
+  )
 }
 
 export default App;
